71cba17be629dd71dbb823569d52b71a1fe36dc6
Prince Kumar <kumar.princepapneja@gmail.com>
2026-03-31 00:51:30 +0530

    firstg

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Routes from './providers/RouterProvider.jsx'
-import StoreProvider from './providers/storeProvider.jsx'
+import StoreProvider from './providers/StoreProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
